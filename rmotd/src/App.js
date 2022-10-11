6eb149dbbf7ef57664b80ff8c9c99f981502500a
Ian MacFarlane <ian@macfarlanehome.com>
2022-10-10 23:08:37 -0700

    Login page demo

--- rmotd/src/App.js
+++ rmotd/src/App.js
@@ -1,7 +1,36 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [token, setToken] = useState();
+
+  if (!token) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Roommate of the Day
+          </p>
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="login-wrapper">
+            <form>
+              <label>
+                <input type="text" placeholder="Username"/>
+              </label>
+              <label>
+                <input type="password" placeholder="Password"/>
+              </label>
+              <div>
+                <button type="submit">Login</button>
+              </div>
+            </form>
+          </div>
+        </header>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
